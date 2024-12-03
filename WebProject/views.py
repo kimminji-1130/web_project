49570110a7197b1285289ea5e41d580c682a743e
kimminji-1130 <ddaa637878@gmail.com>
2024-12-03 19:24:16 +0900

    listline

--- WebProject/views.py
+++ WebProject/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("PBL 프로젝트")
+    data = 'Main page rendered from template'
+    context = {'message': data}
+    return render(request, 'index.html', context)
